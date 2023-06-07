c3fe07b8758feba4faa309d102f854157ce63bf6
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-06-07 11:10:10 +0200

    Update Lab_AGX_202223_S2_skeleton.py

--- Lab_AGX_202223_S2_skeleton.py
+++ Lab_AGX_202223_S2_skeleton.py
@@ -1,5 +1,7 @@
 import networkx as nx
 import pandas as pd
+import numpy as np
+
 from Lab_AGX_202223_S2_skeleton import *
 
 # ------- IMPLEMENT HERE ANY AUXILIARY FUNCTIONS NEEDED ------- #
@@ -79,7 +81,6 @@ def prune_low_degree_nodes(g: nx.Graph, min_degree: int, out_filename: str) -> n
     # ----------------- END OF FUNCTION --------------------- #
 
 
-
 def prune_low_weight_edges(g: nx.Graph, min_weight=None, min_percentile=None, out_filename: str = None) -> nx.Graph:
     """
     Prune a graph by removing edges with weight < threshold. Threshold can be specified as a value or as a percentile.
@@ -91,7 +92,64 @@ def prune_low_weight_edges(g: nx.Graph, min_weight=None, min_percentile=None, ou
     :return: a pruned networkx graph.
     """
     # ------- IMPLEMENT HERE THE BODY OF THE FUNCTION ------- #
-    pass
+    
+    # Check if at least one lower bound is specified
+    if (min_weight == None) and (min_percentile == None):
+
+        #if no bound is specified raise an error
+        raise ValueError("Unspecified bound, please sepecify either min_weight or min_percentile")
+
+    #Check if both bounds are specified
+    elif (min_weight != None) and (min_percentile !=None):
+
+        #If so, raise an error 
+        raise ValueError("Too many specified bounds, please sepecify only one of { min_weight, min_percentile}")
+
+    # Make a copy of the input graph. This is done to avoid altering the original graph.
+    prunned_g = g.copy()
+
+    # List to store the nodes to be pruned
+    edges_to_prune = []
+
+    #List to store weights
+    weights = [data['weight'] for u, v, data in prunned_g.edges(data=True)]
+
+    # Check If the argument min_percentile is not None
+    if min_percentile !=None:
+        #If so use np.percentile to get the threshold weight
+        min_weight = np.percentile(weights, min_percentile)
+
+    #iterate over the nodes in the graph
+    for u, v, data in prunned_g.edges(data=True):
+        
+        #get their weight
+        weight = data['weight']
+        
+        #check if the weight of the edge is lower than the threshold
+        if weight < min_weight:
+
+            #if so, add the edge to the edges_to_prune list
+            edges_to_prune.append((u,v))
+
+    # Iterate over the list of edges to be pruned
+    for u,v in edges_to_prune:
+
+        # Remove each edge from the graph
+        prunned_g.remove_edge(u,v)
+
+    # Iterate over each node in the pruned graph
+    for node in prunned_g:
+
+        # Remove any nodes that have degree 0 after pruning
+        if prunned_g.degree(node) == 0:
+            prunned_g.remove_node(node)
+    
+    # Write the pruned graph to a file in the GraphML format
+    nx.write_graphml(prunned_g, out_filename)
+
+    # Return the pruned graph
+    return prunned_g
+   
     # ----------------- END OF FUNCTION --------------------- #
 
 
